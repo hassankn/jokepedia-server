f49ca447d1ff48aadaeb6040a2ad6118e01b26e8
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-08-09 14:18:23 -0400

    Production deployment additions, CORS issue

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['http://localhost:4200'],
+      origin: [
+        'http://localhost:4200',
+        'https://hassankn.github.io/jokepedia',
+      ],
     },
   });
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
