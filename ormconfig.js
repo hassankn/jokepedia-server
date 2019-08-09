295e2785cea437de0a5f14b9aaf0affb86e0e09e
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-08-09 15:35:20 -0400

    url fix

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     "type": "mysql",
-    "url": "mysql://b58569e7556329:df5cf2e6@us-cdbr-iron-east-02.cleardb.net/heroku_858243da9acdbca?reconnect=true",
+    "url": "mysql://b58569e7556329:df5cf2e6@us-cdbr-iron-east-02.cleardb.net/heroku_858243da9acdbca",
     // "host": "localhost",
     // "port": 3306,
     // "username": "root",
